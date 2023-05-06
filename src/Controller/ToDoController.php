8fb7e98f0ecf98ed6d6446f56ac87def2167c6a7
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-06 13:31:46 +0100

    Part 2 Exerice: add a Todo

--- src/Controller/ToDoController.php
+++ src/Controller/ToDoController.php
@@ -9,17 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoController extends AbstractController
 {
-    #[Route('/todo', name: 'app_to_do')]
+    #[Route('/todo', name: 'todo')]
     public function index(Request $request): Response
     {
 
         $current_session = $request->getSession();
 
-        if($current_session->has('todos')){
-
-            $todos = $current_session->get('todos');
-
-        }else{
+        if (!$current_session->has('todos')) {
 
             $todos = [
                 "achat" => "acheter une clé USB",
@@ -27,10 +23,36 @@ class ToDoController extends AbstractController
                 "correction" => "corriger les examens"
             ];
 
+            $current_session->set('todos', $todos);
+            $this->addFlash('info', "La liste des todo vient d'être initialisée");
         }
 
-        $current_session->set('todos', $todos);
 
         return $this->render('to_do/index.html.twig');
+    }
+
+    //Add a Todo
+    #[Route('/todo/add/{name}/{content}', name: 'todo.add')]
+    public function addToDo(Request $request, $name, $content)
+    {
+
+        $session = $request->getSession();
+
+        if ($session->has('todos')) {
+
+            $todos = $session->get('todos');
+            if(isset($todos[$name])){
+                $this->addFlash('danger', "La liste des todo contient ce ToDo d'id $name");
+            }else{
+                $todos[$name] = $content;
+                $session->set('todos', $todos);
+
+                $this->addFlash('success', "le ToDo d'id $name a été ajouté avec succès");
+            }
+        } else {
+            $this->addFlash('danger', "La liste des todo n'est pas encore initialisée");
+        }
+
+        return $this->redirectToRoute('todo');
     }
 }
