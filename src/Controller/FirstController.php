e24e44b0751aca5b0d008a95540959dbf90a64a2
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-06 16:59:51 +0100

    Route requirements v1

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use PhpParser\Node\Name;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,17 @@ class FirstController extends AbstractController
         return $this->render('first/index.html.twig', [
             'controller_name' => 'FirstController',
         ]);
+    }
+
+    #[Route(
+        '/multi/{entier1}/{entier2}',
+        name: 'multiplication',
+        requirements: ['entier1'=>'\d+', 'entier2'=>'\d+']
+        )] //attribute not an annotation
+    public function multiplication($entier1, $entier2): Response
+    {
+
+        $result = $entier1 * $entier2;
+        return new Response("<h1>$result</h1>");
     }
 }
